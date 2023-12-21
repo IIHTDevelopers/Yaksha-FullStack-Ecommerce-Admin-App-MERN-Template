8cd6aac8f3ff59ac299af1ceb7945630e1277d1f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-21 09:59:32 +0530

    Delete Backend directory

--- Backend/modules/product/dao/models/product.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const productSchema = require("../schemas/product.schema");
-
-const Product = mongoose.model("Product", productSchema);
-
-module.exports = Product;
--- Backend/modules/product/service/product.service.js
+++
@@ -1,9 +0,0 @@
-class ProdcutService {
-  async getAllProducts() {}
-  async searchProductByName(name) {}
-  async addProduct(productData) {}
-  async updateProduct(productId, productData) {}
-  async deleteProduct(productId) {}
-}
-
-module.exports = ProdcutService;
